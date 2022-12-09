1382fc08222d19246470f39172da2b7d9f2b44b8
Noor Soreti <noor.said0698@gmail.com>
2022-12-09 15:24:48 -0500

    finished search

--- components/SearchRestaurant.js
+++ components/SearchRestaurant.js
@@ -8,11 +8,10 @@ function RestaurantDetails({name, address, rating}) {
   return(
       <TouchableOpacity>
         <Text>Restaurant: {name}</Text>
-        <Text>Address: {address}</Text>
         <Text>Rating: {rating}</Text>
+        <Text>Address: {address}</Text>
       </TouchableOpacity>  
-  )
-}
+  )}
 
 export default function SearchRestaurant() {
   const [refresh, setRefresh] = React.useState(0);
@@ -23,17 +22,18 @@ export default function SearchRestaurant() {
 
   const toggleText = () => setShow(show => !show)
 
+
+
   const getRestaurantList = async () => {
-    const tt = "restaurants%20in%20Toronto"
-    const data = await fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${tt}&type=restaurant&location=43.6532%2c79.3832&key=AIzaSyAYUSjjwuGFc39sHx0iQs9oGcETmzAEesk`)
+    const query = searchText.toLowerCase()
+    const data = await fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=Toronto%${query}&type=restaurant&location=43.6532%2c79.3832&radius=10000&key=AIzaSyAYUSjjwuGFc39sHx0iQs9oGcETmzAEesk`)
     const json = await data.json()
     setRestaurants(json.results)
     setLoading(false)
-}
+  }
   
   React.useEffect(() => {
     getRestaurantList().catch(console.error)
-    console.log(restaurantList);
   },[getRestaurantList])
 
   return (
@@ -41,14 +41,14 @@ export default function SearchRestaurant() {
       { isLoading ? <ActivityIndicator/> : (
         <>
       <TextInput
-        onChangeText={ss => setText(ss)}
+        onChangeText={text => setText(text)}
         value={searchText}
         style={{height:40,margin:20}}
       />
       <Button
         title="Go to Details"
         onPress={toggleText}>Search</Button>
-          <FlatList
+            <FlatList
             data={restaurantList}
             renderItem={({item}) => (
               <RestaurantDetails 
@@ -56,47 +56,11 @@ export default function SearchRestaurant() {
                 address={item.formatted_address}
                 rating={item.rating}
               />
-            )
-          }
+            )}
             keyExtractor={item=>item.place_id}
             extraData={refresh}
           />
         </>
       )}
     </SafeAreaView>
-  )
-}
-
-
-
-/*
- <FlatList
-          data={restaurantList}
-          renderItem={({item}) => {
-            return(
-              <RestaurantDetails
-              name={item._z.results.name}
-              address={item._z.results.address}
-              rating={item._z.results.rating}
-              />
-            )
-          }}
-        />
-
-
-
-
-
-
-        <FlatList
-          data={restaurantList}
-          renderItem={({item}) => {
-            console.log("why");
-            return(
-              <Text>bye</Text>
-            )
-          }}
-          keyExtractor={item=>item.id}
-          extraData={refresh}
-        />
-*/
+  )}
