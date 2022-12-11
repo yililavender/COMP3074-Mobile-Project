edb79a2306f037525266ce4ba4fc3cf2e19b5199
yililavender <99084714+yililavender@users.noreply.github.com>
2022-12-11 17:05:12 -0500

    Update RateRestaurtants.js
    Update RateRestaurants.js

--- components/RateRestaurtants.js
+++ components/RateRestaurtants.js
@@ -1,49 +1,113 @@
-import HomeScreen from "./components/HomeScreen";
-import SearchRestaurants from "./components/SearchRestaurants";
-import RateRestaurtants from "./components/RateRestaurtants";
-import MyFavorites from "./components/MyFavorites";
-import RestaurantVisited from "./components/RestaurantVisited";
-import AboutUs from "./components/AboutUs";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {SafeAreaView,StyleSheet,View,Text,Image,TouchableOpacity,ImageBackground} from 'react-native';
+import React, {useState} from 'react';
 
-const Stack = createNativeStackNavigator();
+const RateRestaurtants = () => {
+  const [selectStar, setSelectStar] = useState(3);
+  const [maxStar, setMaxStar] = useState([1, 2, 3, 4, 5]);
 
-export default function App() {
+  const filledStar =
+    'https://raw.githubusercontent.com/AboutReact/sampleresource/master/star_filled.png';
+
+  const emptyStar =
+    'https://raw.githubusercontent.com/AboutReact/sampleresource/master/star_corner.png';
+
+  const Rate = () => {
+    return (
+      <View style={styles.starStyle}>
+        {maxStar.map((item, key) => {
+          return (
+            <TouchableOpacity
+              activeOpacity={0.5}
+              key={item}
+              onPress={() => setSelectStar(item)}>
+              <Image
+                style={styles.starImageStyle}
+                source={
+                  item <= selectStar
+                    ? {uri: filledStar}
+                    : {uri: emptyStar}
+                }
+              />
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    );
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name='Home'
-          component={HomeScreen}
-          options={{title: "Welcome"}}
-        />
-        <Stack.Screen
-          name='SearchRestaurants'
-          component={SearchRestaurants}
-          options={{title: "Search Restaurants"}}
-        />
-        <Stack.Screen
-          name='RateRestaurtants'
-          component={RateRestaurtants}
-          options={{title: "Rate Restaurtants"}}
-        />
-        <Stack.Screen
-          name='MyFavorites'
-          component={MyFavorites}
-          options={{title: "My Favorites"}}
-        />
-        <Stack.Screen
-          name='RestaurantVisited'
-          component={RestaurantVisited}
-          options={{title: "Restaurant Visited"}}
-        />
-        <Stack.Screen
-          name='AboutUs'
-          component={AboutUs}
-          options={{title: "About Us"}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+          style={{ flex: 5 }}
+          source={require('../assets/homepage.png') }
+      >
+      <Image
+          source={require('../assets/icon.png')}
+          style={{ width: 40, height: 40, marginTop: 90 }}
+      />            
+      <Text style={styles.titleText}>Rate Restaurant</Text>
+      <Text style={styles.titleText}>Rate Restaurant</Text>
+      <Text style={styles.textStyle}> Rate Restaurant!</Text>
+
+      <Rate />
+      <Text style={styles.textStyle}>
+          {selectStar} / {Math.max.apply(null, maxStar)}
+      </Text>
+      <TouchableOpacity
+            activeOpacity={0.7}
+            style={styles.buttonStyle}
+            onPress={() => alert('Thanks for your participation!')}>        
+      <Text style={styles.buttonTextStyle}>Send</Text>
+      </TouchableOpacity>
+
+    </ImageBackground>
+  </SafeAreaView>
   );
-}
+};
+
+export default RateRestaurtants;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 10,
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  titleText: {
+    padding: 8,
+    fontSize: 16,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontSize: 23,
+    color: '#000',
+    marginTop: 15,
+  },
+
+  buttonStyle: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 30,
+    padding: 15,
+    backgroundColor: '#a9c2ff',
+  },
+  buttonTextStyle: {
+    color: 'black',
+    textAlign: 'center',
+  },
+  starStyle: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 30,
+  },
+  starImageStyle: {
+    width: 40,
+    height: 40,
+    resizeMode: 'cover',
+  },
+});
+
