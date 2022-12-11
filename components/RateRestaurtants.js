0425a40ef02a4266cad7a7467967892243804169
yililavender <99084714+yililavender@users.noreply.github.com>
2022-12-11 16:27:06 -0500

    Update RateRestaurtants.js
    Rate Restaurant updates

--- components/RateRestaurtants.js
+++ components/RateRestaurtants.js
@@ -1,22 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { StackActions } from '@react-navigation/native';
+import HomeScreen from "./components/HomeScreen";
+import SearchRestaurants from "./components/SearchRestaurants";
+import RateRestaurtants from "./components/RateRestaurtants";
+import MyFavorites from "./components/MyFavorites";
+import RestaurantVisited from "./components/RestaurantVisited";
+import AboutUs from "./components/AboutUs";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function RateRestaurtants({ navigation, route }) {
+const Stack = createNativeStackNavigator();
 
+export default function App() {
   return (
-        
-    
-      <StatusBar style="auto" />
-    
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Home'
+          component={HomeScreen}
+          options={{title: "Welcome"}}
+        />
+        <Stack.Screen
+          name='SearchRestaurants'
+          component={SearchRestaurants}
+          options={{title: "Search Restaurants"}}
+        />
+        <Stack.Screen
+          name='RateRestaurtants'
+          component={RateRestaurtants}
+          options={{title: "Rate Restaurtants"}}
+        />
+        <Stack.Screen
+          name='MyFavorites'
+          component={MyFavorites}
+          options={{title: "My Favorites"}}
+        />
+        <Stack.Screen
+          name='RestaurantVisited'
+          component={RestaurantVisited}
+          options={{title: "Restaurant Visited"}}
+        />
+        <Stack.Screen
+          name='AboutUs'
+          component={AboutUs}
+          options={{title: "About Us"}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
