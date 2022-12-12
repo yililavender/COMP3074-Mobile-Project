2cd44fe0a8e9a5a4fcf36f30fc245a7ec3f4505b
yililavender <99084714+yililavender@users.noreply.github.com>
2022-12-11 20:09:04 -0500

    update my favourite.js

--- components/MyFavorites.js
+++ components/MyFavorites.js
@@ -1,20 +1,32 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function MyFavorites({ navigation, route }) {
+import React from "react";
 
-  return (
-    <View>
-      <Text>My Favorites</Text>
-    </View>   
-  );
+class MyFavorites extends React.Component {
+  render() {
+    const fav = this.props.restaurantsfav.map(item => (
+      <div className="listrestaurant" key={item.id}>
+        <div className="imagenya">
+          <img className="imgstyle" src={item.image} alt={item.name} />
+        </div>
+        <div className="descnya">
+          <h3>Name : {item.name}</h3>
+          <p>
+            <br />
+            <span>Location: {item.location}</span>
+            <button onClick={() => this.props.delete(item.id)}>
+              Delete from Favorite
+            </button>
+          </p>
+        </div>
+      </div>
+    ));
+    return (
+      <div>
+        <h2>Favorite</h2>
+        {fav}
+      </div>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default MyFavorites;
